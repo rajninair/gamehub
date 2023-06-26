8613e6e16b97942adc380e2bcfc0f47918879ee9
rajni.algorisys <rajni.algorisys@ext.idfy.com>
2023-06-26 17:36:16 +0530

    Created a generic data fetching hook

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,45 +1,16 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+// import { useEffect, useState } from "react";
+// import apiClient from "../services/api-client";
+// import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Genre {
   id: number;
   name: string;
 }
-interface FetchGenresResponse {
-  count: number;
-  results: Genre[];
-}
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setIsLoading(true);
-    apiClient
-      .get<FetchGenresResponse>("/genres", {
-        signal: controller.signal,
-      })
-      .then((res) => {
-        setIsLoading(false);
-        setGenres(res.data.results);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) {
-          // console.log("Cancelling request...");
-          return;
-        }
-        setError(err.message);
-        setIsLoading(false);
-      });
-    return () => {
-      controller.abort();
-    };
-  }, []);
-
-  return { genres, error, isLoading };
-};
+// interface FetchGenresResponse {
+//   count: number;
+//   results: Genre[];
+// }
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
