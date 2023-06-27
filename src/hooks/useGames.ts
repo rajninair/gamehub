f3af3e4445c22e7706b101b3dc4d9efbc6918a7f
rajni.algorisys <rajni.algorisys@ext.idfy.com>
2023-06-27 17:20:32 +0530

    Filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,9 +18,19 @@ export interface Game {
 //   count: number;
 //   results: Game[];
 // }
-const useGames = (selectedGenre: Genre | null) =>
-  useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
-    selectedGenre?.id,
-  ]);
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    {
+      params: {
+        genres: selectedGenre?.id,
+        platforms: selectedPlatform?.id,
+      },
+    },
+    [selectedGenre?.id, selectedPlatform?.id] // dependencies
+  );
 
 export default useGames;
